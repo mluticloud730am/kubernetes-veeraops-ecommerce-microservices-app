6dc9a690c0cd9e69d305809282f7bcb12b1d7589
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-05-20 21:56:49 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -41,7 +41,10 @@ app.config.update(
     MAIL_USE_TLS=True,
     MAIL_USERNAME=os.getenv("MAIL_USERNAME", ""),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD", ""),
+    MAIL_DEFAULT_SENDER=os.getenv("MAIL_USERNAME", "")
+    )
 )
+
 mail = Mail(app)
 
 
